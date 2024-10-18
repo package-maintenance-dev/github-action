1930f278ed99eb5d61a3b5518bd6acc58e9f4841
IvannKurchenko <ivankoorchenko@gmail.com>
2024-10-18 12:38:57 +0200

    implemented additional parsing and validation

--- action/main.py
+++ action/main.py
@@ -31,18 +31,45 @@ def parse_arguments():
     return parser.parse_args()
 
 
+def parse_sbom_scores_thresholds(thresholds_str):
+    thresholds_dict = {}
+    for entry in thresholds_str.split(","):
+        key, value = map(str.strip, entry.split(":"))
+        if not key or not value:
+            raise ValueError("Both key and value must be non-empty.")
+        thresholds_dict[key] = value.upper()
+    return thresholds_dict
+
+
 def main():
     args = parse_arguments()
+
+    try:
+        owner, repo = map(str.strip, args.github_repository.split("/"))
+        if not owner or not repo:
+            raise ValueError("Both owner and repo must be non-empty.")
+    except ValueError:
+        raise ValueError(
+            "Invalid format for GitHub repository. It should be in the form 'owner/repo'."
+        )
 
     if args.sbom_ignore_packages:
         sbom_ignore_packages = args.sbom_ignore_packages.split("\n")
     else:
         sbom_ignore_packages = None
 
-    print("GitHub Repository:", args.github_repository)
+    if args.sbom_scores_thresholds:
+        sbom_scores_thresholds = parse_sbom_scores_thresholds(
+            args.sbom_scores_thresholds
+        )
+    else:
+        sbom_scores_thresholds = None
+
+    print("GitHub Repository Owner:", owner)
+    print("GitHub Repository Name:", repo)
     print("GitHub Token:", args.github_token)
     print("SBOM Ignore Packages:", sbom_ignore_packages)
-    print("SBOM Scores Thresholds:", args.sbom_scores_thresholds)
+    print("SBOM Scores Thresholds:", sbom_scores_thresholds)
 
 
 if __name__ == "__main__":
--- tests/test_main.py
+++ tests/test_main.py
@@ -2,8 +2,21 @@ import pytest
 import sys
 from unittest.mock import patch
 
-from action.main import parse_arguments
+from action.main import parse_sbom_scores_thresholds, parse_arguments
 
+
+def test_parse_sbom_scores_thresholds():
+    thresholds_str = 'source.commit.frequency:B,binary.release.recency:A'
+    expected_dict = {
+        'source.commit.frequency': 'B',
+        'binary.release.recency': 'A'
+    }
+    result = parse_sbom_scores_thresholds(thresholds_str)
+    assert result == expected_dict
+
+def test_invalid_sbom_scores_thresholds():
+    with pytest.raises(ValueError, match="Both key and value must be non-empty."):
+        parse_sbom_scores_thresholds('source.commit.frequency:,binary.release.recency:A')
 
 def test_parse_arguments():
     test_args = [
@@ -16,11 +29,16 @@ def test_parse_arguments():
 
     with patch.object(sys, 'argv', test_args):
         args = parse_arguments()
-
-        assert args.github_repository == 'owner/repo'
+        owner, repo = args.github_repository.split('/')
+        assert owner.strip() == 'owner'
+        assert repo.strip() == 'repo'
         assert args.github_token == 'testtoken'
         assert args.sbom_ignore_packages == 'pkg1\npkg2\npkg3'
-        assert args.sbom_scores_thresholds == 'source.commit.frequency:B,binary.release.recency:A'
+        sbom_scores_thresholds = parse_sbom_scores_thresholds(args.sbom_scores_thresholds)
+        assert sbom_scores_thresholds == {
+            'source.commit.frequency': 'B',
+            'binary.release.recency': 'A'
+        }
 
 def test_missing_required_argument():
     test_args = [
@@ -44,10 +62,16 @@ def test_no_ignore_packages():
     with patch.object(sys, 'argv', test_args):
         args = parse_arguments()
 
-        assert args.github_repository == 'owner/repo'
+        owner, repo = args.github_repository.split('/')
+        assert owner.strip() == 'owner'
+        assert repo.strip() == 'repo'
         assert args.github_token == 'testtoken'
         assert args.sbom_ignore_packages is None
-        assert args.sbom_scores_thresholds == 'source.commit.frequency:B,binary.release.recency:A'
+        sbom_scores_thresholds = parse_sbom_scores_thresholds(args.sbom_scores_thresholds)
+        assert sbom_scores_thresholds == {
+            'source.commit.frequency': 'B',
+            'binary.release.recency': 'A'
+        }
 
 def test_multiline_ignore_packages():
     test_args = [
@@ -61,7 +85,8 @@ def test_multiline_ignore_packages():
         args = parse_arguments()
         ignore_packages = args.sbom_ignore_packages.split('\n')
 
-        assert args.github_repository == 'owner/repo'
-        assert args.github_token == 'testtoken'
+        owner, repo = args.github_repository.split('/')
+        assert owner.strip() == 'owner'
+        assert repo.strip() == 'repo'
         assert ignore_packages == ['pkg1', 'pkg2', 'pkg3']
         assert args.sbom_scores_thresholds is None
